1b881aebcb5345fb488eb5ceab6884f2bb6c4b5d
Edward Boorn <edwardboorn@Edwards-MacBook-Pro.local>
2020-03-30 19:49:59 +0100

    added social links

--- src/components/layout/navBar.js
+++ src/components/layout/navBar.js
@@ -6,10 +6,13 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
-import { withStyles } from '@material-ui/core/styles';
-import Divider from '@material-ui/core/Divider';
-
-import me from './../../me.jpg'
+import { withStyles } from "@material-ui/core/styles";
+import Divider from "@material-ui/core/Divider";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import me from "./../../me.jpg";
 
 import AboutMe from "../pages/aboutMe";
 import Contact from "../pages/contact";
@@ -18,6 +21,7 @@ import Skills from "../pages/skills";
 import WorkExperience from "../pages/workExperience";
 import PersonalProjects from "../pages/personalProjects";
 import { createBrowserHistory } from "history";
+
 
 const history = createBrowserHistory();
 
@@ -30,7 +34,7 @@ const useStyles = makeStyles(theme => ({
   drawer: {
     width: drawerWidth,
     flexShrink: 0,
-    maxWidth : drawerWidth
+    maxWidth: drawerWidth
   },
   drawerPaper: {
     width: drawerWidth
@@ -40,96 +44,120 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: theme.palette.background.default,
     padding: theme.spacing(3)
   },
-  aboutContent : {
-    paddingLeft : '40%',
+  aboutContent: {
+    paddingLeft: "40%"
   }
 }));
 
-const Navigation = withStyles(useStyles) (
-    ({classes}) => (
-        <Router history={history}>
-        <Drawer
-          className={classes.drawer}
-          variant="permanent"
-          classes={{
-            paper: classes.drawerPaper
-          }}
-          anchor="left"
-        >
-          <div className="personalIntro">
-            <img alt="This is me " src={me} className="navImageStyle"/>
-            <Divider variant="middle"/>
-          <h1>Edward Boorn</h1>
-          <h2 className={classes.aboutContent} style={{padding:"5%"}} > Product // Tech // Space </h2>
-          <Divider variant="middle"/>
-          </div>
-          <div className={classes.toolbar} />
-          <List>
-            {/* <ListItem
+const Navigation = withStyles(useStyles)(({ classes }) => (
+  <Router history={history}>
+    <Drawer
+      className={classes.drawer}
+      variant="permanent"
+      classes={{
+        paper: classes.drawerPaper
+      }}
+      anchor="left"
+    >
+      <div className="personalIntro">
+        <img alt="This is me " src={me} className="navImageStyle" />
+        <Divider variant="middle" />
+        <h1>Edward Boorn</h1>
+        <h2 className={classes.aboutContent} style={{ padding: "5%" }}>
+          {" "}
+          Product // Tech // Space{" "}
+        </h2>
+        <Divider variant="middle" />
+      </div>
+      <div className={classes.toolbar} />
+      <List>
+        {/* <ListItem
               button
               component={Link}
               to="/home"
             >
               <ListItemText>Home</ListItemText>
             </ListItem> */}
-            <ListItem
-              button
-              component={Link}
-              to="/aboutMe"
-            >
-              <ListItemText>About Me</ListItemText>
-            </ListItem>
-            <ListItem
-              button
-              component={Link}
-              to="/workExperience"
-            >
-              <ListItemText>Job History</ListItemText>
-            </ListItem>
-            <ListItem
-              button
-              component={Link}
-              to="/education"
-            >
-              <ListItemText>Education</ListItemText>
-            </ListItem>
-            <ListItem
-              button
-              component={Link}
-              to="/skills"
-            >
-              <ListItemText>Skills</ListItemText>
-            </ListItem>
-            <ListItem
-              button
-              component={Link}
-              to="/personalProjects"
-            >
-              <ListItemText>Personal Projects</ListItemText>
-            </ListItem>
-            {/* <ListItem
+        <ListItem button component={Link} to="/aboutMe">
+          <ListItemText>About Me</ListItemText>
+        </ListItem>
+        <ListItem button component={Link} to="/workExperience">
+          <ListItemText>Job History</ListItemText>
+        </ListItem>
+        <ListItem button component={Link} to="/education">
+          <ListItemText>Education</ListItemText>
+        </ListItem>
+        <ListItem button component={Link} to="/skills">
+          <ListItemText>Skills</ListItemText>
+        </ListItem>
+        <ListItem button component={Link} to="/personalProjects">
+          <ListItemText>Personal Projects</ListItemText>
+        </ListItem>
+        {/* <ListItem
               button
               component={Link}
               to="/contact"
             >
               <ListItemText>Contact</ListItemText>
             </ListItem> */}
-          </List>
-        </Drawer>
-        <main className={classes.content}>
-          <Route exact path="/portfolio/" >{
-            <Redirect to="/aboutMe"/>
-          }</Route>
-          <Route path="/aboutMe" component={AboutMe}/>
-          <Route path="/contact" component={Contact}/>
-          <Route path="/education" component={Education}/>
-          <Route path="/skills" component={Skills}/>
-          <Route path="/workExperience" component={WorkExperience}/>
-          <Route path="/personalProjects" component={PersonalProjects}/>
-        </main>
-        </Router>
-    )
-)
+      </List>
+      <Divider />
+      <div className="socials">
+        <div className="socialItem">
+          <a
+            className="socialItemLink"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/edboorn"
+          >
+            <GitHubIcon fontSize="large" />
+          </a>
+        </div>
+        <div className="socialItem">
+          <a
+            className="socialItemLink"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.instagram.com/edboorn/"
+          >
+            <InstagramIcon fontSize="large" />
+          </a>
+        </div>
+        <div className="socialItem">
+          <a
+            className="socialItemLink"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://twitter.com/ed_boorn"
+          >
+            <TwitterIcon fontSize="large" />
+          </a>
+        </div>
+        <div className="socialItem">
+          <a
+            className="socialItemLink"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.linkedin.com/in/edward-boorn/"
+          >
+            <LinkedInIcon fontSize="large" />
+          </a>
+        </div>
+      </div>
+    </Drawer>
+    <main className={classes.content}>
+      <Route exact path="/portfolio/">
+        {<Redirect to="/aboutMe" />}
+      </Route>
+      <Route path="/aboutMe" component={AboutMe} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/education" component={Education} />
+      <Route path="/skills" component={Skills} />
+      <Route path="/workExperience" component={WorkExperience} />
+      <Route path="/personalProjects" component={PersonalProjects} />
+    </main>
+  </Router>
+));
 
 export default function PermanentDrawerLeft() {
   const classes = useStyles();
@@ -137,7 +165,7 @@ export default function PermanentDrawerLeft() {
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <Navigation/>
+      <Navigation />
     </div>
   );
 }
