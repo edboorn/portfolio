24cb16c6971348ed796d37cbbffa076bc21dddbb
Edward Boorn <edward.boorn@btinternet.com>
2020-09-20 11:28:35 +0100

    Updates

--- precache-manifest.f77c30ec80e049d05eb3f19a18610154.js
+++ precache-manifest.f77c30ec80e049d05eb3f19a18610154.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "08d4c0b9690540c5ee477e719a27b8d3",
+    "revision": "dc02686acdc8974c04ae6bc780f6d0de",
     "url": "/portfolio/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/css/2.ab965100.chunk.css"
   },
   {
-    "revision": "cf384afb8b22e9c262eb",
-    "url": "/portfolio/static/css/main.e8cfadfa.chunk.css"
+    "revision": "d691a636081b5692b98b",
+    "url": "/portfolio/static/css/main.ba2db9e2.chunk.css"
   },
   {
     "revision": "555e3f1a70d2a08eae4f",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.ac1bdc6c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "cf384afb8b22e9c262eb",
-    "url": "/portfolio/static/js/main.321b6579.chunk.js"
+    "revision": "d691a636081b5692b98b",
+    "url": "/portfolio/static/js/main.4ba0f25b.chunk.js"
   },
   {
     "revision": "7107900f59be1fd95810",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.745ee056180d30b4fee4191d679d0aca.js"
+  "/portfolio/precache-manifest.f77c30ec80e049d05eb3f19a18610154.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
