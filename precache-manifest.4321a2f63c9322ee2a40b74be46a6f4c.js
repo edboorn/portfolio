8c4aec1b308478767165412bccf28fb8739c657e
Edward Boorn <edward.boorn@btinternet.com>
2022-09-04 20:04:23 +0100

    Updates

---
+++ precache-manifest.4321a2f63c9322ee2a40b74be46a6f4c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ab4e90cb3d1a9693be083c4aadb389d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b48f9bbf0cff96a65e03",
+    "url": "/portfolio/static/css/2.0eaf8e31.chunk.css"
+  },
+  {
+    "revision": "ea2ed86af96a8b87a48a",
+    "url": "/portfolio/static/css/main.e8cfadfa.chunk.css"
+  },
+  {
+    "revision": "b48f9bbf0cff96a65e03",
+    "url": "/portfolio/static/js/2.ac1bdc6c.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/portfolio/static/js/2.ac1bdc6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea2ed86af96a8b87a48a",
+    "url": "/portfolio/static/js/main.d8c3e2ba.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  },
+  {
+    "revision": "910320c1b986870ac1c3a139ea0442ef",
+    "url": "/portfolio/static/media/me2.910320c1.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.764c051e5693ceb0b5bc9d20b29c8fc2.js"
+  "/portfolio/precache-manifest.4321a2f63c9322ee2a40b74be46a6f4c.js"
 );
 
 self.addEventListener('message', (event) => {
